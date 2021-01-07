d957c39709075e5dd4d4192b3e9a3a3e7f921dd4
Angel D. Munoz <scythes.lol@gmail.com>
2021-01-07 00:33:28 -0700

    update auth routes include expiresAt in jwt

--- src/AuthRoutes.kt
+++ src/AuthRoutes.kt
@@ -9,7 +9,8 @@ import io.ktor.response.*
 import io.ktor.routing.*
 import com.toxicbakery.bcrypt.Bcrypt
 import io.ktor.server.engine.*
-import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 import java.util.*
 
 
@@ -18,6 +19,22 @@ fun Route.authRoutes(env: ApplicationEnvironment) {
     val algorithm = Algorithm.HMAC256(env.config.property("jwt.secret").getString())
     val audience = env.config.property("jwt.audience").getString()
     val issuer = env.config.property("jwt.issuer").getString()
+
+    fun signToken(email: String): String {
+        val now = LocalDateTime.now()
+        val date = Date.from(now.toInstant(ZoneOffset.UTC))
+        val tomorrow = now.plusDays(1)
+        val expiresAt = Date.from(tomorrow.toInstant(ZoneOffset.UTC))
+        return JWT
+            .create()
+            .withClaim("email", email)
+            .withIssuer(issuer)
+            .withAudience(audience)
+            .withExpiresAt(expiresAt)
+            .withIssuedAt(date)
+            .withNotBefore(date)
+            .sign(algorithm) ?: throw FailedToCreateJwtException("Failed to create JWT Token")
+    }
 
     post("/auth/login") {
         val payload =
@@ -29,34 +46,31 @@ fun Route.authRoutes(env: ApplicationEnvironment) {
             }
         if (payload == null) {
             call.response.status(HttpStatusCode.BadRequest)
-            call.respond(FailAuthResponse("Failed to receive credentials"))
+            call.respond(FailedRequestResponse("Failed to receive credentials"))
             return@post
         }
         val user = LoginPayload.findByEmail(payload.email.toLowerCase())
 
         if (user == null) {
             call.response.status(HttpStatusCode.BadRequest)
-            call.respond(FailAuthResponse("Invalid Credentials"))
+            call.respond(FailedRequestResponse("Invalid Credentials"))
             return@post
         }
 
         val isValid = Bcrypt.verify(payload.password, user.password.toByteArray())
 
         if (isValid) {
-            val token =
-                JWT
-                    .create()
-                    .withClaim("email", user.email)
-                    .withIssuer(issuer)
-                    .withAudience(audience)
-                    .withIssuedAt(Date.from(Instant.now()))
-                    .withNotBefore(Date.from(Instant.now()))
-                    .sign(algorithm)
-
-            call.respond(AuthResponse(user.email, token))
-            return@post
+            try {
+                val token = signToken(user.email)
+                call.respond(AuthResponse(user.email, token))
+                return@post
+            } catch (e: FailedToCreateJwtException) {
+                call.response.status(HttpStatusCode.InternalServerError)
+                call.respond(FailedRequestResponse("An unexpected error just happened"))
+                return@post
+            }
         }
-        call.respond(FailAuthResponse("Invalid Credentials"))
+        call.respond(FailedRequestResponse("Invalid Credentials"))
     }
 
     post("/auth/signup") {
@@ -69,37 +83,34 @@ fun Route.authRoutes(env: ApplicationEnvironment) {
             }
         if (payload == null) {
             call.response.status(HttpStatusCode.BadRequest)
-            call.respond(FailAuthResponse("Failed to receive signup data"))
+            call.respond(FailedRequestResponse("Failed to receive signup data"))
             return@post
         }
 
         val exists = User.checkExists(email = payload.email)
 
-        if(exists) {
+        if (exists) {
             call.response.status(HttpStatusCode.BadRequest)
-            call.respond(FailAuthResponse("The User Already Exists"))
+            call.respond(FailedRequestResponse("The User Already Exists"))
             return@post
         }
         val hash = Bcrypt.hash(payload.password, 12)
-        val preSave = payload.copy(email = payload.email.toLowerCase(), password = String(hash, Charsets.UTF_8) )
+        val preSave = payload.copy(email = payload.email.toLowerCase(), password = String(hash, Charsets.UTF_8))
         val created = SignupPayload.createUser(preSave)
 
-        if(!created) {
+        if (!created) {
             call.response.status(HttpStatusCode.InternalServerError)
-            call.respond(FailAuthResponse("Failed to create the user"))
+            call.respond(FailedRequestResponse("Failed to create the user"))
             return@post
         }
-        val token =
-            JWT
-                .create()
-                .withClaim("email", preSave.email)
-                .withIssuer(issuer)
-                .withAudience(audience)
-                .withIssuedAt(Date.from(Instant.now()))
-                .withNotBefore(Date.from(Instant.now()))
-                .sign(algorithm)
-        call.response.status(HttpStatusCode.Created)
-        call.respond(AuthResponse(preSave.email, token))
-        return@post
+        try {
+            val token = signToken(preSave.email)
+            call.response.status(HttpStatusCode.Created)
+            call.respond(AuthResponse(preSave.email, token))
+            return@post
+        } catch(e:FailedToCreateJwtException) {
+            call.response.status(HttpStatusCode.InternalServerError)
+            call.respond(FailedRequestResponse("An unexpected error just happened"))
+        }
     }
 }
