e81ea1c30a8df87bad9eaab2a4bcf50aa5d4f12e
Angel D. Munoz <scythes.lol@gmail.com>
2021-01-07 00:42:43 -0700

    move into its own function

--- src/Profiles.kt
+++ src/Profiles.kt
@@ -7,17 +7,21 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.util.pipeline.*
 
 fun Route.profileRoutes(env: ApplicationEnvironment) {
+    suspend fun PipelineContext<Unit, ApplicationCall>.extractUser(): User {
+        val principal =
+            call.principal<JWTPrincipal>() ?: throw InvalidClaimValuesException("No Principal in the request")
+        val email =
+            principal.payload.getClaim("email")
+                ?: throw InvalidClaimValuesException("The email is missing from the jwt")
+        return User.findByEmail(email.asString()) ?: throw UserNotFoundException("Can't find the user from the jwt")
+    }
+
     get("profiles") {
         try {
-            val principal =
-                call.principal<JWTPrincipal>() ?: throw InvalidClaimValuesException("No Principal in the request")
-            val email =
-                principal.payload.getClaim("email")
-                    ?: throw InvalidClaimValuesException("The email is missing from the jwt")
-            val user =
-                User.findByEmail(email.asString()) ?: throw UserNotFoundException("Can't find the user from the jwt")
+            val user = extractUser()
             call.respond(Profile.findProfiles(user._id))
         } catch (e: InvalidClaimValuesException) {
             call.response.status(HttpStatusCode.UnprocessableEntity)
@@ -34,20 +38,14 @@ fun Route.profileRoutes(env: ApplicationEnvironment) {
                 ?: throw InvalidContentException("Body was not present in the request")
             val name =
                 params["name"] ?: throw InvalidContentException("the profile name was not present in the request")
-            val principal =
-                call.principal<JWTPrincipal>() ?: throw InvalidClaimValuesException("No Principal in the request")
-            val email =
-                principal.payload.getClaim("email")
-                    ?: throw InvalidClaimValuesException("The email is missing from the jwt")
-            val user =
-                User.findByEmail(email.asString()) ?: throw UserNotFoundException("Can't find the user from the jwt")
+            val user = extractUser()
             if (Profile.profileExists(name)) {
                 call.response.status(HttpStatusCode.BadRequest)
                 call.respond(FailedRequestResponse("The Profile Already Exists"))
                 return@post
             }
             val created = Profile.insertProfile(ProfilePayload(user._id, name))
-            if(!created) {
+            if (!created) {
                 call.response.status(HttpStatusCode.InternalServerError)
                 call.respond(FailedRequestResponse("Failed to create the profile"))
                 return@post
